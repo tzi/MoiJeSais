f355c752d2a3d08d84dd781b9baad0f106564509
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-16 16:06:52 +0300

    Correction orthographique

--- messages/answer/4.php
+++ messages/answer/4.php
@@ -1,5 +1,5 @@
 Hmmm je vois...<br>
-Par contre il va falloir ce concentrer un peu plus...<br>
+Par contre il va falloir se concentrer un peu plus...<br>
 <br>
 Prends 2s pour donner un peu d'oxygène à ton cerveau cellulaire.<br>
 <a href="/">Et dis-moi quand tu es prêt</a>
