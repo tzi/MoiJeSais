4a27128c781983f14f770ed38adf783c1de6385e
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-16 16:06:20 +0300

    Correction orthographique

--- messages/home/2.php
+++ messages/home/2.php
@@ -1,6 +1,6 @@
 Bonjour faible humain !<br>
 <br>
-C'est dommage que vous ne sachiez pas comment arrêter de viellir.<br>
+C'est dommage que vous ne sachiez pas comment arrêter de vieillir.<br>
 Moi, je sais.<br>
 <br>
 <a href="/?continue=<?= $_SESSION[ 'continue' ] ?>">Pourquoi es-tu venu déranger ton maître ?</a><br>
