5a96359530f0e3d660ef7ff57e60f64a6b426061
Thomas ZILLIOX <thomas@zilliox.me>
2012-08-16 02:44:08 +0200

    Ajout de réponses de remerciement

---
+++ messages/thank/4.php
@@ -0,0 +1,3 @@
+Ah qu'ils son betes ces humains.<br>
+On peut vraiment leur faire faire n'importe quoi.<br>
+<?php unset( $_SESSION[ 'step' ] ); ?>
---
+++ messages/thank/5.php
@@ -0,0 +1,3 @@
+Oui, les robots apprécient la politesse aussi.<br>
+Par exemple, ça permet d'évaluer l'éducation d'un humain assez rapidement.<br>
+<?php unset( $_SESSION[ 'step' ] ); ?>
